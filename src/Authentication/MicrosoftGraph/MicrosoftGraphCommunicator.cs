80b716bb3e41f4334b582f00109f515bc43cd055
carlossanlop <1175054+carlossanlop@users.noreply.github.com>
2023-04-06 19:25:51 -0600

    Minor fixes for MicrosoftGraph project
    - Include Authentication in namespace
    - Switch to file-scoped namespaces
    - Make cache file name more specific

--- src/Authentication/MicrosoftGraph/MicrosoftGraphCommunicator.cs
+++ src/Authentication/MicrosoftGraph/MicrosoftGraphCommunicator.cs
@@ -6,55 +6,54 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 // Some of this code was obtained from Azure-Samples/active-directory-dotnetcore-devicecodeflow-v2
-namespace ServicingTools.MicrosoftGraph
+namespace ServicingTools.Authentication.MicrosoftGraph;
+
+/// <summary>
+/// Allows executing Microsoft Graph queries.
+/// </summary>
+public class MicrosoftGraphCommunicator
 {
+    private const string JsonMediaType = "application/json";
+
+    private readonly string _graphBaseEndpoint;
+    private readonly AuthenticationResult _authResult;
+    private readonly HttpClient _httpClient;
+
     /// <summary>
-    /// Allows executing Microsoft Graph queries.
+    /// Initializes a <see cref="MicrosoftGraphCommunicator"/> instance.
     /// </summary>
-    public class MicrosoftGraphCommunicator
+    /// <param name="authResult">A previously generated authentication object, containing a valid authentication state to Microsoft Graph.</param>
+    /// <param name="graphBaseEndpoint">The URL string representing the endpoint and Graph version.</param>
+    internal MicrosoftGraphCommunicator(AuthenticationResult authResult, string graphBaseEndpoint)
     {
-        private const string JsonMediaType = "application/json";
+        ArgumentException.ThrowIfNullOrEmpty(graphBaseEndpoint);
+        ArgumentNullException.ThrowIfNull(authResult);
+        _graphBaseEndpoint = graphBaseEndpoint;
+        _authResult = authResult;
 
-        private readonly string _graphBaseEndpoint;
-        private readonly AuthenticationResult _authResult;
-        private readonly HttpClient _httpClient;
-
-        /// <summary>
-        /// Initializes a <see cref="MicrosoftGraphCommunicator"/> instance.
-        /// </summary>
-        /// <param name="authResult">A previously generated authentication object, containing a valid authentication state to Microsoft Graph.</param>
-        /// <param name="graphBaseEndpoint">The URL string representing the endpoint and Graph version.</param>
-        internal MicrosoftGraphCommunicator(AuthenticationResult authResult, string graphBaseEndpoint)
+        _httpClient = new HttpClient();
+        if (!_httpClient.DefaultRequestHeaders.Accept.Any(m => m.MediaType == JsonMediaType))
         {
-            ArgumentException.ThrowIfNullOrEmpty(graphBaseEndpoint);
-            ArgumentNullException.ThrowIfNull(authResult);
-            _graphBaseEndpoint = graphBaseEndpoint;
-            _authResult = authResult;
-
-            _httpClient = new HttpClient();
-            if (!_httpClient.DefaultRequestHeaders.Accept.Any(m => m.MediaType == JsonMediaType))
-            {
-                _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(JsonMediaType));
-            }
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", _authResult.AccessToken);
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(JsonMediaType));
         }
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", _authResult.AccessToken);
+    }
 
-        /// <summary>
-        /// Executes a Graph API query.
-        /// Examples:
-        /// - Retrieve the current user's information: 'me'.
-        /// - Retrieve the current user's manager's information: 'me/manager'.
-        /// </summary>
-        /// <param name="api">Represents the arguments of the query, which come after the base endpoint URL and the version.</param>
-        /// <returns>A boolean-string tuple where the boolean value indicates whether the status code was success or not, and the string contains the response content.</returns>
-        public async Task<(bool, string?)> TryCallAsync(string api)
-        {
-            ArgumentException.ThrowIfNullOrEmpty(api);
-            string query = $"{_graphBaseEndpoint}{api}";
-            HttpResponseMessage response = await _httpClient.GetAsync(query);
-            bool success = response.IsSuccessStatusCode;
-            string result = await response.Content.ReadAsStringAsync();
-            return (success, result);
-        }
+    /// <summary>
+    /// Executes a Graph API query.
+    /// Examples:
+    /// - Retrieve the current user's information: 'me'.
+    /// - Retrieve the current user's manager's information: 'me/manager'.
+    /// </summary>
+    /// <param name="api">Represents the arguments of the query, which come after the base endpoint URL and the version.</param>
+    /// <returns>A boolean-string tuple where the boolean value indicates whether the status code was success or not, and the string contains the response content.</returns>
+    public async Task<(bool, string?)> TryCallAsync(string api)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(api);
+        string query = $"{_graphBaseEndpoint}{api}";
+        HttpResponseMessage response = await _httpClient.GetAsync(query);
+        bool success = response.IsSuccessStatusCode;
+        string result = await response.Content.ReadAsStringAsync();
+        return (success, result);
     }
 }

09554e1d5f8aeb6c9a966eb24a642144e8ec37c2
carlossanlop <1175054+carlossanlop@users.noreply.github.com>
2023-04-17 17:06:38 -0600

    Outlook Communicator with meeting creation.

---
+++ src/Connectors/OfficeInterop/MeetingOptions.cs
@@ -0,0 +1,17 @@
+﻿using Outlook = Microsoft.Office.Interop.Outlook;
+using System;
+using System.Collections.Generic;
+
+namespace InfrastructureTools.Connectors.OfficeInterop;
+
+public class MeetingOptions
+{
+    public string Location { get; set; } = string.Empty;
+    public string Subject { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+    public DateTime Start { get; set; } = DateTime.Now;
+    public int Duration { get; set; } = 30;
+    public Outlook.OlImportance Importance { get; set; } = Outlook.OlImportance.olImportanceNormal;
+    public Outlook.OlBusyStatus BusyStatus { get; set; } = Outlook.OlBusyStatus.olBusy;
+    public List<string> Recipients { get; set; } = new List<string>();
+}
---
+++ src/Connectors/OfficeInterop/OutlookCommunicator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace InfrastructureTools.Connectors.OfficeInterop;
+
+public class OutlookCommunicator
+{
+    public OutlookCommunicator()
+    {
+        if (Process.GetProcessesByName("OUTLOOK").Length > 0)
+        {
+            Application = GetActiveObject("Outlook.Application") as Outlook.Application ??
+                throw new NullReferenceException("Could not retrieve the existing Outlook Application instance.");
+        }
+        else
+        {
+            Application = new Outlook.Application();
+            Outlook.NameSpace? ns = Application.GetNamespace("MAPI");
+            ns.Logon("", "", Missing.Value, Missing.Value);
+        }
+    }
+
+    public Outlook.Application Application { get; }
+
+    public void CreateMeeting(MeetingOptions options)
+    {
+        Outlook.AppointmentItem appt =
+            (Outlook.AppointmentItem)Application.CreateItem(Outlook.OlItemType.olAppointmentItem) ??
+            throw new NullReferenceException("Could not create an appointment item.");
+
+        appt.MeetingStatus = Outlook.OlMeetingStatus.olNonMeeting;
+        appt.Location = options.Location;
+        appt.Subject = options.Subject;
+        appt.Body = options.Body;
+        appt.Start = options.Start;
+        appt.Duration = options.Duration;
+        appt.Importance = options.Importance;
+        appt.BusyStatus = options.BusyStatus;
+
+        foreach (string recipientEmail in options.Recipients)
+        {
+            Outlook.Recipient recipient = appt.Recipients.Add(recipientEmail);
+            recipient.Type = (int)Outlook.OlMeetingRecipientType.olRequired;
+        }
+
+        appt.Display();
+    }
+
+    private static object GetActiveObject(string progId)
+    {
+        ArgumentNullException.ThrowIfNull(progId);
+
+        int hr = CLSIDFromProgIDEx(progId, out Guid clsid);
+        if (hr < 0)
+        {
+            Marshal.ThrowExceptionForHR(hr);
+        }
+
+        hr = GetActiveObject(clsid, IntPtr.Zero, out object obj);
+        if (hr < 0)
+        {
+            Marshal.ThrowExceptionForHR(hr);
+        }
+
+        return obj;
+    }
+
+    [DllImport("ole32")]
+    private static extern int CLSIDFromProgIDEx([MarshalAs(UnmanagedType.LPWStr)] string lpszProgID, out Guid lpclsid);
+
+    [DllImport("oleaut32")]
+    private static extern int GetActiveObject([MarshalAs(UnmanagedType.LPStruct)] Guid rclsid, IntPtr pvReserved, [MarshalAs(UnmanagedType.IUnknown)] out object ppunk);
+}
